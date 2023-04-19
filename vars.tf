81ce35e6c7800a77292d696d6af10e564126df89
kiran1616 <kirankumar7163@gmail.com>
2023-04-19 16:09:19 +0530

    updated script

--- vars.tf
+++ vars.tf
@@ -2,6 +2,7 @@ variable "env" {}
 variable "subnet_ids" {}
 variable "vpc_id" {}
 variable "allow_cidr" {}
+variable "engine_version" {}
+variable "num_cache_nodes" {}
 variable "node_type" {}
-variable "replicas_per_node_group" {}
-variable "num_node_groups" {}
+
