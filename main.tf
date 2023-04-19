da736cf66550a71bb10fa0d3ab109156bfd3ef96
kiran1616 <kirankumar7163@gmail.com>
2023-04-19 16:33:14 +0530

    updated script

--- main.tf
+++ main.tf
@@ -60,20 +60,17 @@ resource "aws_security_group" "elasticache" {
 #}
 
 
-resource "aws_elasticache_cluster" "elastic_cache" {
-  cluster_id           = "${var.env}-elasticache"
-  engine               = "redis"
-  node_type            = var.node_type
-  num_cache_nodes      = var.num_cache_nodes
-  engine_version       = var.engine_version
-  port                 = 6379
-  subnet_group_name = aws_elasticache_subnet_group.default.name
+resource "aws_elasticache_cluster" "elasticache" {
+  cluster_id         = "${var.env}-elasticache-cluster"
+  engine             = "redis"
+  node_type          = var.node_type
+  num_cache_nodes    = var.num_cache_nodes
+  engine_version     = var.engine_version
+  port               = 6379
+  subnet_group_name  = aws_elasticache_subnet_group.default.name
   security_group_ids = [aws_security_group.elasticache.id]
-
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-elasticache " }
+    { Name = "${var.env}-elasticache" }
   )
-
 }
-
